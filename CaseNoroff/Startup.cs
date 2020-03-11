1a6d212cada0b81f47e81456af4c661a01485f6d
kristgn <snawklakl@gmail.com>
2020-03-11 14:34:14 +0100

    Extracted login razor page

--- CaseNoroff/Startup.cs
+++ CaseNoroff/Startup.cs
@@ -11,6 +11,7 @@ using CaseNoroff.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace CaseNoroff
 {
