1a4fcb27dd45cf3de0f79218098de1784e2cc680
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-03-11 13:59:57 +0100

    Computed column

---
+++ CaseNoroff/Data/Migrations/20200311125449_computedTotalPrice.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CaseNoroff.Data.Migrations
+{
+    public partial class computedTotalPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "OrderItems",
+                type: "decimal(18, 2)",
+                nullable: false,
+                computedColumnSql: "(CONVERT([decimal](18,2),[dbo].[getTotalPrice]([OrderItemId])))",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "OrderItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)",
+                oldComputedColumnSql: "(CONVERT([decimal](18,2),[dbo].[getTotalPrice]([OrderItemId])))");
+        }
+    }
+}
