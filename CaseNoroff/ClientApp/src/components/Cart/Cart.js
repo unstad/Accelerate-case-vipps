4d2f88430004ee65af251d19682faa1f415df70c
Lap Vinh Nguyen <vinh.nguyen@no.experis.com>
2020-03-16 15:00:34 +0100

    Cart Update With Delete Button

---
+++ CaseNoroff/ClientApp/src/components/Cart/Cart.js
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import { CartItem } from '../CartItem/CartItem.js'
+import styles from './Cart.css'
+
+export class Cart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            itemList: JSON.parse(localStorage.getItem('cartList'))
+        }
+    }
+
+    removeItem = (item, event) => {
+        console.log("Remove clicked")
+        
+        let list = [...this.state.itemList];
+        let index = list.indexOf(event.target.value);
+        console.log(item);
+        for (let i = 0; i < list.length; i++) {
+            if (list[i] == item) {
+                list.splice(i, 1)
+            }
+        }
+        console.log(list);
+        localStorage.setItem('cartList', JSON.stringify(list));
+        this.setState({ itemList: list })
+        console.log(this.state.itemList);
+    }
+
+    render() {
+        const list = this.state.itemList.map(item => {
+            
+            return (
+                <li className = 'cartLi' key={item.productId}>
+                    <CartItem item={item} buttonClick={this.removeItem.bind(null, item)}/>
+                </li>
+                )
+        })
+        return (
+            <div>
+                <h2 id= 'cartTitle'>Cart</h2>
+                <ul id='cartContainer'>
+                    {list}
+                </ul>
+            </div>
+            )
+    }
+}
---
+++ CaseNoroff/ClientApp/src/components/CartItem/CartItem.js
@@ -0,0 +1,46 @@
+﻿import React from 'react';
+import { Button } from 'reactstrap';
+import styles from './CartItem.css';
+
+export class CartItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.item.productId,
+            name: this.props.item.productName,
+            price: this.props.item.price,
+            image: this.props.item.imgURL,
+            //sizes: this.props.item.sizes,
+        }
+    }
+
+    render() {
+        return (
+            /*<div className='itemContainer'>
+                <Card className = 'card'>
+                    <CardImg className='itemImage' top width="100%" src={this.state.image} alt={this.state.name} />
+                    <CardBody>
+                        <CardTitle>{this.state.name}</CardTitle>
+                        <CardSubtitle>price</CardSubtitle>
+                        <CardText>NOK {this.state.price},-</CardText>
+                        <Button  onClick={this.props.buttonClick}>Remove from cart</Button>
+                    </CardBody>
+                </Card>
+            </div>*/
+
+            <div className='cartContainer'>
+                <img className='cartItemImage' top width="100%" src={this.state.image} alt={this.state.name} />
+                <div className='cartItemText'>
+                    <div className='cartItemName'>
+                        <h3>{this.state.name}</h3>
+                    </div>
+                    <div className = 'cartItemPrice'>
+                        <h5>price</h5>
+                        <p>NOK {this.state.price},-</p>
+                        <Button className='delBtn' onClick={this.props.buttonClick}>Remove from cart</Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
