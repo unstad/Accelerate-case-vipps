600bd3f3a782efc1e80bf19912893199efa8ec82
kristgn <snawklakl@gmail.com>
2020-03-17 17:18:53 +0100

    Fixed cart error

--- CaseNoroff/ClientApp/src/components/Shop/Shop.js
+++ CaseNoroff/ClientApp/src/components/Shop/Shop.js
@@ -15,7 +15,7 @@ export class Shop extends React.Component {
 
     async componentDidMount() {
         this.setState({itemList: [], filteredItemList: [] })
-        const api_url = `https://localhost:44364/ECommerce/Product`
+        const api_url = `https://localhost:5001/ECommerce/Product`
         try {
             const response = await fetch(api_url).then(resp => resp.json());
             let list = [...this.state.itemList];
@@ -37,13 +37,11 @@ export class Shop extends React.Component {
 
     addItem = (item, event) => {
         let list = []
-        if (localStorage.getItem('cartList') != 0) {
+        if (localStorage.getItem('cartList') != null) {
             list = [...this.state.addedItems];
         }
         list.push(item);
-        console.log(list);
         this.setState({ addedItems: list });
-        console.log(this.state.addedItems)
         localStorage.setItem('cartList', JSON.stringify(list));
     }
 
