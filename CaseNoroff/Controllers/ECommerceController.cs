3307cac0068eb6bf57c92f7662420ccd478b4dfb
kristgn <snawklakl@gmail.com>
2020-03-19 10:41:08 +0100

    Moved orderconfirmation mail code to temporary class

--- CaseNoroff/Controllers/ECommerceController.cs
+++ CaseNoroff/Controllers/ECommerceController.cs
@@ -44,16 +44,6 @@ namespace CaseNoroff.Controllers
         [HttpGet]
         public List<Customer> Customers()
         {
-			var apiKey = "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";
-			var client = new SendGridClient(apiKey);
-			var from = new EmailAddress("kristian.nilsen@no.experis.com", "Vipps store");
-			var to = new EmailAddress("yakihah538@mailernam.com"); //get user email
-			var subject = "Order confirmation";
-			var plainText = "lmao";
-			var htmlContent = "<strong>Gucci belt</strong>";
-			var msg = MailHelper.CreateSingleEmail(from, to, subject, plainText, htmlContent);
-			client.SendEmailAsync(msg);
-				
             return _db.Customers.ToList();
         }
 
---
+++ CaseNoroff/OrderConfirmationMail.cs
@@ -0,0 +1,11 @@
+//temporary class to hold this code
+public class OrderConfirmation {
+	var client = new SendGridClient("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");
+	var from = new EmailAddress("kristian.nilsen@no.experis.com", "Vipps store");
+	var to = new EmailAddress("yakihah538@mailernam.com"); //get user email
+	var subject = "Order confirmation";
+	var plainText = "lmao"; 
+	var htmlContent = "<strong>Gucci belt</strong>"; //Mail content
+	var msg = MailHelper.CreateSingleEmail(from, to, subject, plainText, htmlContent);
+	client.SendEmailAsync(msg);
+}
