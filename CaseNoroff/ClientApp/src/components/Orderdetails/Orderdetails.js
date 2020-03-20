0dfb10ad68a4552949f567d22e5f1c2fca557824
kristgn <snawklakl@gmail.com>
2020-03-20 14:24:49 +0100

    Fixed form fields

--- CaseNoroff/ClientApp/src/components/Orderdetails/Orderdetails.js
+++ CaseNoroff/ClientApp/src/components/Orderdetails/Orderdetails.js
@@ -23,35 +23,35 @@ export class Orderdetails extends React.Component {
 				<Form onSubmit={this.handleSubmit}>
 					<FormGroup>
 						<Label for="firstName">First name</Label>
-						<Input type="text" name="firstName" id="firstName" value={sessionStorage.firstName || ''} onChange={this.handleChange}/>
+						<Input type="text" name="firstName" id="firstName" onChange={this.handleChange}/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="lastName">Last name</Label>
-						<Input type="text" name="lastName" id="lastName" value={sessionStorage.lastName || ''} onChange={this.handleChange}/>
+						<Input type="text" name="lastName" id="lastName" onChange={this.handleChange}/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="email">Email address</Label>
-						<Input type="email" name="email" id="email" value={sessionStorage.email || ''} onChange={this.handleChange}/>
+						<Input type="email" name="email" id="email" onChange={this.handleChange}/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="phoneNumber">Phone number</Label>
-						<Input type="text" name="phoneNumber" id="phoneNumber" value={sessionStorage.phoneNumber || ''} onChange={this.handleChange}/>
+						<Input type="text" name="phoneNumber" id="phoneNumber" onChange={this.handleChange}/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="address">Address</Label>
-						<Input type="text" name="address" id="address" value={sessionStorage.address || ''} onChange={this.handleChange}/>
+						<Input type="text" name="address" id="address" onChange={this.handleChange}/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="country">Country</Label>
-						<Input type="text" name="country" id="country" value={sessionStorage.country || ''} onChange={this.handleChange}/>
+						<Input type="text" name="country" id="country" onChange={this.handleChange}/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="city">City</Label>
-						<Input type="text" name="city" id="city" value={sessionStorage.city || ''} onChange={this.handleChange}/>
+						<Input type="text" name="city" id="city" onChange={this.handleChange}/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="zipCode">Zip code</Label>
-						<Input type="text" name="zipCode" id="zipCode" value={sessionStorage.zipCode || ''} onChange={this.handleChange}/>
+						<Input type="text" name="zipCode" id="zipCode" onChange={this.handleChange}/>
 					</FormGroup>
 					<Button type="submit" color="primary" size="lg" block onClick={this.handleSubmit}>Continue to payment</Button>
 				</Form>
