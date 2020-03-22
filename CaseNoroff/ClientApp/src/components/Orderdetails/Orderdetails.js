576672086e0b265299ac21b9be9a9c2ce5bc8dd7
kristgn <snawklakl@gmail.com>
2020-03-22 10:14:34 +0100

    Implement user input safety

--- CaseNoroff/ClientApp/src/components/Orderdetails/Orderdetails.js
+++ CaseNoroff/ClientApp/src/components/Orderdetails/Orderdetails.js
@@ -14,6 +14,7 @@ export class Orderdetails extends React.Component {
 	}
 
 	handleChange = (e) => {
+		//disable < and >
 		sessionStorage.setItem(e.target.name, e.target.value)
 	}
 
@@ -23,35 +24,35 @@ export class Orderdetails extends React.Component {
 				<Form onSubmit={this.handleSubmit}>
 					<FormGroup>
 						<Label for="firstName">First name</Label>
-						<Input type="text" name="firstName" id="firstName" onChange={this.handleChange}/>
+						<Input type="text" name="firstName" id="firstName" maxlength="50" onChange={this.handleChange}/ required>
 					</FormGroup>
 					<FormGroup>
 						<Label for="lastName">Last name</Label>
-						<Input type="text" name="lastName" id="lastName" onChange={this.handleChange}/>
+						<Input type="text" name="lastName" id="lastName" maxlength="50" onChange={this.handleChange} required/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="email">Email address</Label>
-						<Input type="email" name="email" id="email" onChange={this.handleChange}/>
+						<Input type="email" name="email" id="email" maxlength="50" onChange={this.handleChange} required/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="phoneNumber">Phone number</Label>
-						<Input type="text" name="phoneNumber" id="phoneNumber" onChange={this.handleChange}/>
+						<Input type="text" name="phoneNumber" id="phoneNumber" maxlength="20" onChange={this.handleChange} required/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="address">Address</Label>
-						<Input type="text" name="address" id="address" onChange={this.handleChange}/>
+						<Input type="text" name="address" id="address" maxlength="50" onChange={this.handleChange} required/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="country">Country</Label>
-						<Input type="text" name="country" id="country" onChange={this.handleChange}/>
+						<Input type="text" name="country" id="country" maxlength="50" onChange={this.handleChange} required/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="city">City</Label>
-						<Input type="text" name="city" id="city" onChange={this.handleChange}/>
+						<Input type="text" name="city" id="city" maxlength="50" onChange={this.handleChange} required/>
 					</FormGroup>
 					<FormGroup>
 						<Label for="zipCode">Zip code</Label>
-						<Input type="text" name="zipCode" id="zipCode" onChange={this.handleChange}/>
+						<Input type="text" name="zipCode" id="zipCode" maxlength="50" onChange={this.handleChange} required/>
 					</FormGroup>
 					<Button type="submit" color="primary" size="lg" block onClick={this.handleSubmit}>Continue to payment</Button>
 				</Form>
