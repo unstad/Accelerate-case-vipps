f0b87268fba434ec3ef923a51fa79862eb658107
Lap Vinh Nguyen <vinh.nguyen@no.experis.com>
2020-03-26 14:54:58 +0100

    responsive orderhistory

--- CaseNoroff/ClientApp/src/components/OrderHistory/OrderHistoryItem.js
+++ CaseNoroff/ClientApp/src/components/OrderHistory/OrderHistoryItem.js
@@ -15,14 +15,22 @@ export default class OrderHistoryItem extends React.Component {
         for (let i = 0; i < order.orderItems.length; i++) {
             total += order.orderItems[i].totalPrice;
         }
+        const images = this.props.order.orderItems.map(item => {
+            return (
+                <div className='orderHistoryImgSet'>
+                    <img className='historyImg' src={item.product.imgURL} alt={item.product.productName} />
+                    <p> X{item.productQuantity}</p>
+                </div>
+                )
+        })
         return (
                 <div className='historyItem'>
-                    <div className = 'historyItemText'>
-                        <h3>Order date: {date} </h3>
+                <div className='historyItemText'>
+                    <h3 className = 'orderDate'>Order date: {date} </h3>
                         <p><strong>Order number: </strong>{order.orderId}</p>
                         <p><strong>Delivery address: </strong>{order.deliveryAddress.streetAddress} {order.deliveryAddress.country}, {order.deliveryAddress.city} {order.deliveryAddress.postalCode}</p>
                         <p><strong>Total: </strong>NOK {total},-</p>
-                        <img className='historyImg' src={order.orderItems[0].product.imgURL} alt={order.orderItems[0].product.productName} /> 
+                        <ul className = 'historyImgContainer'>{images}</ul>
                     </div>
                 </div>
             )
