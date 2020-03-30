70ba4d30516c1ec92bce78e2e1141c5adc32d28c
Katharina <k_unstad@hotmail.com>
2020-03-30 13:29:50 +0200

    Add Error404 and Switch routes

---
+++ CaseNoroff/ClientApp/src/components/Error404/Error404.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+export class Error404 extends React.Component {
+
+    render() {
+        return (
+            <div>
+                404... The site you requested cannot be found!
+            </div>
+            )
+    }
+}
