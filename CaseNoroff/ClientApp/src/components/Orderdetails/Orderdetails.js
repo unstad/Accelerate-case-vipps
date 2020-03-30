c1040ff7b9fea740bbde09d5c6d636b704fcf4aa
Katharina <k_unstad@hotmail.com>
2020-03-30 13:38:27 +0200

    Orderdetails: email disabled

--- CaseNoroff/ClientApp/src/components/Orderdetails/Orderdetails.js
+++ CaseNoroff/ClientApp/src/components/Orderdetails/Orderdetails.js
@@ -204,6 +204,13 @@ export class Orderdetails extends React.Component {
 			<div>
 				<Form onSubmit={this.handleSubmit}>
 					<FormGroup>
+						<Label for="email">Email address</Label>
+						<Input type="email" name="email" id="email" maxLength="50"
+							onChange={this.handleChange.bind(this, "email")}
+							value={this.state.fields.email || ""} required disabled/>
+						<span style={{ color: "red" }}>{this.state.errors["email"]}</span>
+					</FormGroup>
+					<FormGroup>
 						<Label for="firstName">First name</Label>
 						<Input type="text" name="firstName" id="firstName" maxLength="25"
 							   onChange={this.handleChange.bind(this, "firstName")}
@@ -216,13 +223,6 @@ export class Orderdetails extends React.Component {
 							   onChange={this.handleChange.bind(this, "lastName")}
 							   value={this.state.fields.lastName  || ""} required/>
 						<span style={{color: "red"}}>{this.state.errors["lastName"]}</span>
-					</FormGroup>
-					<FormGroup>
-						<Label for="email">Email address</Label>
-						<Input type="email" name="email" id="email" maxLength="50"
-							   onChange={this.handleChange.bind(this, "email")}
-							   value={this.state.fields.email  || ""} required/>
-						<span style={{color: "red"}}>{this.state.errors["email"]}</span>
 					</FormGroup>
 					<FormGroup>
 						<Label for="phoneNumber">Phone number</Label>
